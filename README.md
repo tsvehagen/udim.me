a06f2b6c50775b5ac615df22f0273078619aefc2
Tobias Svehagen <tobias.svehagen@gmail.com>
2017-10-05 18:20:00 +0200

    Add site

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# udim.me
+# [udim.me](http://udim.me) website
+
+### About
+
+udim.me is a project I made for controlling a lamp over BLE with design in mind. The lamp is connected to a dimmer-box which in turn connects to the power outlet. The lamp can then be turned on/off or dimmed with a wooden puck.
+
+* The PCBs were designed with EagleCAD and manufactured by [OSHPark](https://oshpark.com/)
+* Both the puck and dimmer-box are using a nRF52 chip running [Zephyr RTOS](https://www.zephyrproject.org/)
+* The puck uses the Bosch BMI160 for gyro and accelerometer
+* The dimmer-box uses triac and zero-crossing detection for dimming
+* A transformerless power supply based on capacitive full-wave rectification is used in the dimmer-box to get from 230 VAC to 3.3 VDC.
+
+### Images
+
+<p float="left">
+    <img src="/src/img/gallery/puck-dark-sm.jpg" width="270" hspace="10" />
+    <img src="/src/img/gallery/puck-box-sm.jpg" width="270" hspace="10" />
+    <img src="/src/img/gallery/pcb-sm.jpg" width="270" hspace="10" />
+</p>
---
+++ src/js/main.js
@@ -0,0 +1,236 @@
+/* ---------------------------------------------- /*
+ * Preloader
+ /* ---------------------------------------------- */
+(function() {
+    $(window).on('load', function() {
+        $('.loader').fadeOut();
+        $('.page-loader').delay(350).fadeOut('slow');
+    });
+
+    $(document).ready(function() {
+
+        /* ---------------------------------------------- /*
+         * WOW Animation When You Scroll
+         /* ---------------------------------------------- */
+
+        wow = new WOW({
+            mobile: false
+        });
+        wow.init();
+
+
+        /* ---------------------------------------------- /*
+         * Scroll top
+         /* ---------------------------------------------- */
+
+        $(window).scroll(function() {
+            if ($(this).scrollTop() > 100) {
+                $('.scroll-up').fadeIn();
+            } else {
+                $('.scroll-up').fadeOut();
+            }
+        });
+
+        $('a[href="#totop"]').click(function() {
+            $('html, body').animate({ scrollTop: 0 }, 'slow');
+            return false;
+        });
+
+
+        /* ---------------------------------------------- /*
+         * Initialization General Scripts for all pages
+         /* ---------------------------------------------- */
+
+        var homeSection = $('.home-section'),
+            navbar      = $('.navbar-custom'),
+            navHeight   = navbar.height(),
+            worksgrid   = $('#works-grid'),
+            width       = Math.max($(window).width(), window.innerWidth),
+            mobileTest  = false;
+
+        if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+            mobileTest = true;
+        }
+
+        buildHomeSection(homeSection);
+        navbarAnimation(navbar, homeSection, navHeight);
+
+        $(window).resize(function() {
+            var width = Math.max($(window).width(), window.innerWidth);
+            buildHomeSection(homeSection);
+        });
+
+        $(window).scroll(function() {
+            effectsHomeSection(homeSection, this);
+            navbarAnimation(navbar, homeSection, navHeight);
+        });
+
+        /* ---------------------------------------------- /*
+         * Set sections backgrounds
+         /* ---------------------------------------------- */
+
+        var module = $('.home-section, .module, .module-small, .side-image');
+        module.each(function(i) {
+            if ($(this).attr('data-background')) {
+                $(this).css('background-image', 'url(' + $(this).attr('data-background') + ')');
+		if (mobileTest) {
+			$(this).css('background-attachment', 'scroll');
+		}
+            }
+        });
+
+        /* ---------------------------------------------- /*
+         * Home section height
+         /* ---------------------------------------------- */
+
+        function buildHomeSection(homeSection) {
+            if (homeSection.length > 0) {
+                if (homeSection.hasClass('home-full-height')) {
+                    homeSection.height($(window).height());
+                } else {
+                    homeSection.height($(window).height() * 0.85);
+                }
+            }
+        }
+
+
+        /* ---------------------------------------------- /*
+         * Home section effects
+         /* ---------------------------------------------- */
+
+        function effectsHomeSection(homeSection, scrollTopp) {
+            if (homeSection.length > 0) {
+                var homeSHeight = homeSection.height();
+                var topScroll = $(document).scrollTop();
+                if ((homeSection.hasClass('home-parallax')) && ($(scrollTopp).scrollTop() <= homeSHeight)) {
+                    homeSection.css('top', (topScroll * 0.55));
+                }
+                if (homeSection.hasClass('home-fade') && ($(scrollTopp).scrollTop() <= homeSHeight)) {
+                    var caption = $('.caption-content');
+                    caption.css('opacity', (1 - topScroll/homeSection.height() * 1));
+                }
+            }
+        }
+
+        /* ---------------------------------------------- /*
+         * Transparent navbar animation
+         /* ---------------------------------------------- */
+
+        function navbarAnimation(navbar, homeSection, navHeight) {
+            var topScroll = $(window).scrollTop();
+            if (navbar.length > 0 && homeSection.length > 0) {
+                if(topScroll >= navHeight) {
+                    navbar.removeClass('navbar-transparent');
+                } else {
+                    navbar.addClass('navbar-transparent');
+                }
+            }
+        }
+
+        /* ---------------------------------------------- /*
+         * Video popup, Gallery
+         /* ---------------------------------------------- */
+
+        $('.video-pop-up').magnificPopup({
+            type: 'iframe'
+        });
+
+        $(".gallery-item").magnificPopup({
+            delegate: 'a',
+            type: 'image',
+            gallery: {
+                enabled: true,
+                navigateByImgClick: true,
+                preload: [0,1]
+            },
+            image: {
+                titleSrc: 'title',
+                tError: 'The image could not be loaded.'
+            }
+        });
+
+
+        /* ---------------------------------------------- /*
+         * Portfolio
+         /* ---------------------------------------------- */
+
+        var worksgrid   = $('#works-grid'),
+            worksgrid_mode;
+
+        if (worksgrid.hasClass('works-grid-masonry')) {
+            worksgrid_mode = 'masonry';
+        } else {
+            worksgrid_mode = 'fitRows';
+        }
+
+        worksgrid.imagesLoaded(function() {
+            worksgrid.isotope({
+                layoutMode: worksgrid_mode,
+                itemSelector: '.work-item'
+            });
+        });
+
+        /* ---------------------------------------------- /*
+         * Progress bar animations
+         /* ---------------------------------------------- */
+
+        $('.progress-bar').each(function(i) {
+            $(this).appear(function() {
+                var percent = $(this).attr('aria-valuenow');
+                $(this).animate({'width' : percent + '%'});
+                $(this).find('span').animate({'opacity' : 1}, 900);
+                $(this).find('span').countTo({from: 0, to: percent, speed: 900, refreshInterval: 30});
+            });
+        });
+
+        /* ---------------------------------------------- /*
+         * Scroll Animation
+         /* ---------------------------------------------- */
+
+        $('.section-scroll').bind('click', function(e) {
+            var anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $(anchor.attr('href')).offset().top - 50
+            }, 1000);
+            e.preventDefault();
+        });
+
+        /*===============================================================
+         Interest Form
+         ================================================================*/
+
+        $("#interest-form").submit(function (e) {
+            e.preventDefault();
+            var $ = jQuery;
+
+            var postData = $(this).serializeArray(),
+                formURL = $(this).attr("action"),
+                $formResponse = $('#form-rsp'),
+                $formSubmit = $("#form-submit"),
+                formSubmitText = $formSubmit.text();
+
+            $formSubmit.text("Sending...");
+
+            $.ajax(
+                {
+                    url: formURL,
+                    type: "POST",
+                    data: postData,
+                    success: function (data) {
+                        $formResponse.html(data);
+                        $formSubmit.text(formSubmitText);
+                        $('#interest-form input[name=name]').val('');
+                        $('#interest-form input[name=email]').val('');
+                        $('#interest-form textarea[name=message]').val('');
+                    },
+                    error: function (data) {
+                        alert("An error occured, please try again.");
+                    }
+                });
+
+            return false;
+        });
+    });
+})(jQuery);
+
+
